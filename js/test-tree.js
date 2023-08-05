e426f41d336cab928b59058fa299dac6e5c619f7
Pham Minh Luan <minhluanpham1309@gmail.com>
2023-08-05 11:10:12 +0700

    update new render tree

--- js/test-tree.js
+++ js/test-tree.js
@@ -142,167 +142,98 @@ var filterTree = function(objectTree){
   return objectTree;
 }
 
-var renderObject = function (arr) {
-  let result = [];
-  let level = {result};
-  arr.forEach(urlE => {
-    const url = new URL(urlE);
-    let path = removeProtocol(urlE);
-    path.split("/").reduce((r, name, i, a) => {
-      if (name !== "" || (a.length >= 2)) {
-        if(!r[name]) {
-          r[name] = {result: []};
-          let namePath = makePath(i, a);
-          r.result.push({NamePath: url.protocol + "/" + namePath + (name.length > 1 ? "/" : ""), Name: "/" + name, Children: r[name].result});
-        }
-        return r[name];
-      }
-    }, level);
-  });
-  return filterTree(result[0]);
-}
-
-var testA = ["https://a/","https://a/b/","https://a/b/b1/","https://a/c"];
-
-console.log(renderObject(testA));
-
-
-
-// var filterTree = function(objectTree){
-//   if((objectTree.Children.length <= 1 && objectTree.Name !== "/")) {
-//     objectTree.Children = [];
-//   }
-//   objectTree.Children.forEach(e => {
-//     filterTree(e);
-//   });
-//   return objectTree;
-// }
-
-// var  testArr = "mieru-ca.com/heatmap/what-is-heatmap".split("/");
-
-// var testarr = ["/abc","/tyui","/","nhg"];
-// console.log(testarr);
-
-// var indexOFSlash = function(arr){
-//   let index = 0;
-//   let idxSlash = -1;
-//   arr.forEach(e => {
-//     if(e.Name === "/")
-//     {
-//       idxSlash = index;
-//     }
-//     index +=1;
-//   });
-//   return idxSlash;
-// }
-
-// var permutatorFirstElement = function(inputArr) {
-//   let result = [];
-//   let position = indexOFSlash(inputArr);
-//   result.push(inputArr[position]);
-//   result = result.concat(inputArr);
-//   result.splice(position+1, 1);
-//   return result;
-// } 
-
-
-
-// console.log(permutatorFirstElement(testarr));
-
-
-
-
-//console.log(makePath(1, testArr));
-
-//console.log(result);
-
-
-
-
-// var sortTree = function(objectTree) {
-//   if (objectTree.Children.length === 0) {
-//     return;
-//   }
-
-//   objectTree.Children = permutatorFirstElement(objectTree.Children);
-
-//   objectTree.Children.forEach(e => {
-//     sortTree(e);
-//   });
-//   return objectTree;
-// }
-
-// console.log(sortTree(result[0]));
-
-// filterTree(sortTree(result[0]));
-
-
-// var demoTreeData = JSON.parse('{"Number":"KI-125-25","Name":"All","Children":[{"Number":"WA-775-99","Name":"Main House","Children":[{"Number":"JI-105-09","Name":"Downstairs","Children":[]},{"Number":"TR-883-66","Name":"Upstairs","Children":[{"Number":"SS-002-99","Name":"Bedrooms","Children":[{"Number":"JI-656-09","Name":"Master Bedroom","Children":[]},{"Number":"ZZ-654-66","Name":"Guest Bedroom","Children":[]}]},{"Number":"SS-001-99","Name":"Other Rooms","Children":[{"Number":"JI-898-09","Name":"Great Room","Children":[]},{"Number":"ZZ-493-66","Name":"Bonus Room","Children":[]}]}]}]},{"Number":"QQ-542-10","Name":"Garage","Children":[]}]}');
-
-// var treeObject = JSON.stringify(result[0]);
-// var demoTreeData = JSON.parse(treeObject);
-// $('.tree').simpleTreePicker( {
-//   "tree": sortTree(result[0]),
-//   "onclick": function(){		
-//     var selected = $(".tree").simpleTreePicker("val");
-//     selected.forEach(e => {
-//       //console.log(e);
-//     });
-//     // $("#selected").html( !!selected.length ? selected.toString().replace(/,/g,', ') : "Nothing Selected" );
-//   },
-//   "name" : "register-url-list"
-// });
-
-// // Update view with initial state (onclick isn't called for initial selection)
-// $("#selected").html( $(".tree").simpleTreePicker("display").toString().replace(/,/g,', ') );
-
-function generateTreeStructureFromUrls(urlTemplates) {
-  const treeRoot = { result: [] };
-  
-  urlTemplates.forEach(urlTemplate => {
-    const url = new URL(urlTemplate);
-    const pathSegments = removeEmptyPathSegments(getUrlPathSegments(urlTemplate));
-    const currentNode = treeRoot;
-    addPathSegmentsToTree(pathSegments, url.protocol, currentNode, 0);
-  });
-
-  return treeRoot.result;
-}
-
 function getUrlPathSegments(url) {
   const path = removeProtocol(url);
-  return path.split("/");
+  return path.split("/").slice(1);
 }
 
-function removeProtocol(url) {
-  return url.replace(/^(https?|ftp):\/\//, "");
-}
-
-function removeEmptyPathSegments(pathSegments) {
-  return pathSegments.filter(segment => segment !== "");
-}
-
-function addPathSegmentsToTree(pathSegments, protocol, parentNode, index) {
-  if (index === pathSegments.length) {
+function addPathSegmentsToTree(pathSegments, protocol, currentNode, currentPath, currentName) {
+  if (pathSegments.length === 0) {
     return;
   }
 
-  const currentSegment = pathSegments[index];
+  const currentSegment = pathSegments[0];
+  const newPath = currentPath + currentSegment + (currentSegment.length > 1 ? "/" : "");
 
-  if (!parentNode[currentSegment]) {
-    const newNode = { result: [] };
-    const path = getPathFromSegments(pathSegments, index);
-    const newNodePath = protocol + path + (currentSegment.length > 1 ? "/" : "");
-    const newNodeData = { NamePath: newNodePath, Name: "/" + currentSegment, Children: newNode.result };
-    parentNode.result.push(newNodeData);
-    parentNode[currentSegment] = newNode;
+  let existingNode = currentNode.find(node => node.NamePath === newPath);
+  if ((!existingNode) || (existingNode 
+          && pathSegments[pathSegments.length - 1] === currentSegment 
+          && existingNode.Children.length > 0)) {
+    existingNode = { NamePath: newPath, Name: '/'+ currentSegment, Children: [] };
+    currentNode.push(existingNode);
   }
 
-  addPathSegmentsToTree(pathSegments, protocol, parentNode[currentSegment], index + 1);
+  addPathSegmentsToTree(pathSegments.slice(1), protocol, existingNode.Children, newPath, currentSegment);
 }
 
-function getPathFromSegments(pathSegments, index) {
-  const pathSegmentsBeforeCurrent = pathSegments.slice(0, index);
-  return "/" + pathSegmentsBeforeCurrent.join("/") + "/";
+var renderObject = function (urlTemplates) {
+  const treeRoot = { result: [] };
+    
+  urlTemplates.forEach(urlTemplate => {
+    const url = new URL(urlTemplate);
+    const pathSegments = getUrlPathSegments(url.pathname);
+    pathSegments.unshift(url.hostname);
+    const protocol = url.protocol;
+    addPathSegmentsToTree(pathSegments, protocol, treeRoot.result, protocol + "//", '');
+  });
+  
+  return filterTree(sortTree(treeRoot.result[0]));
 }
+
+// var testA = ["https://a/","https://a/b/","https://a/b/b1/","https://a/c"];
+
+// console.log(renderObject(testA));
+
+
+var indexOFSlash = function(arr){
+  let index = 0;
+  let idxSlash = -1;
+  arr.forEach(e => {
+    if(e.Name === "/")
+    {
+      idxSlash = index;
+    }
+    index +=1;
+  });
+  return idxSlash;
+}
+
+var permutatorFirstElement = function(inputArr) {
+  let result = [];
+  let position = indexOFSlash(inputArr);
+  result.push(inputArr[position]);
+  result = result.concat(inputArr);
+  result.splice(position+1, 1);
+  return result;
+} 
+
+var sortTree = function(objectTree) {
+  if (objectTree.Children.length === 0) {
+    return;
+  }
+
+  objectTree.Children = permutatorFirstElement(objectTree.Children);
+
+  objectTree.Children.forEach(e => {
+    sortTree(e);
+  });
+  return objectTree;
+}
+
+$('.tree').simpleTreePicker( {
+  "tree": renderObject(filterByUrl(arr)),
+  "onclick": function(){		
+    var selected = $(".tree").simpleTreePicker("val");
+    selected.forEach(e => {
+      //console.log(e);
+    });
+    // $("#selected").html( !!selected.length ? selected.toString().replace(/,/g,', ') : "Nothing Selected" );
+  },
+  "name" : "register-url-list"
+});
+
+// Update view with initial state (onclick isn't called for initial selection)
+//$("#selected").html( $(".tree").simpleTreePicker("display").toString().replace(/,/g,', ') );
+
+
 
